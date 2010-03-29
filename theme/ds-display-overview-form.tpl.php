ed733629f33b766df487a428141590d683de0658
swentel <swentel@realize.be>
2010-03-29 16:54:41 +0000

    Fix in overview screen

--- theme/ds-display-overview-form.tpl.php
+++ theme/ds-display-overview-form.tpl.php
@@ -17,8 +17,8 @@ if ($rows): ?>
 
 <div id="ds-display-content">
   <div id="ds-tabs">
+    <div id="field-tab" class="tab selected"><a href="javascript:;" onClick="Drupal.DisplaySuite.toggleDisplayTab('field-tab'); return false;"><?php print t('Fields'); ?></a></div>
     <?php if ($sync_copy_tab): ?>
-      <div id="field-tab" class="tab selected"><a href="javascript:;" onClick="Drupal.DisplaySuite.toggleDisplayTab('field-tab'); return false;"><?php print t('Fields'); ?></a></div>
       <div id="sync-copy-tab" class="tab"><a href="javascript:;" onClick="Drupal.DisplaySuite.toggleDisplayTab('sync-copy-tab'); return false;"><?php print t('Sync / copy'); ?></a></div>
     <?php endif; ?>
     <?php if (!empty($plugins_tabs)): ?>
