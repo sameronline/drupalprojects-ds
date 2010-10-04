724d915bf79c83ca7940f9c5dcf2b35f4f27ee12
swentel <swentel@realize.be>
2010-10-04 12:02:01 +0000

    We can drag and drop in regions

---
+++ js/ds.js
@@ -0,0 +1,83 @@
+/* $Id$ */
+
+(function($) {
+
+/**
+ * Row handlers for the 'Manage display' screen.
+ */
+Drupal.fieldUIDisplayOverview = Drupal.fieldUIDisplayOverview || {};
+
+Drupal.fieldUIDisplayOverview.ds = function (row, data) {
+
+  this.row = row;
+  this.name = data.name;
+  this.region = data.region;
+  this.tableDrag = data.tableDrag;
+
+  // Attach change listener to the 'formatter type' select.
+  this.$formatSelect = $('select.field-formatter-type', row);
+  this.$formatSelect.change(Drupal.fieldUIOverview.onChange);
+
+  return this;
+};
+
+Drupal.fieldUIDisplayOverview.ds.prototype = {
+  /**
+   * Returns the region corresponding to the current form values of the row.
+   */
+  getRegion: function () {
+    region = this.$formatSelect.val() == 'hidden'
+    if (region == 'hidden') {
+      return 'hidden';
+    }
+  },
+
+  /**
+   * Reacts to a row being changed regions.
+   *
+   * This function is called when the row is moved to a different region, as a
+   * result of either :
+   * - a drag-and-drop action (the row's form elements then probably need to be
+   *   updated accordingly)
+   * - user input in one of the form elements watched by the
+   *   Drupal.fieldUIOverview.onChange change listener.
+   *
+   * @param region
+   *   The name of the new region for the row.
+   * @return
+   *   A hash object indicating which rows should be AJAX-updated as a result
+   *   of the change, in the format expected by
+   *   Drupal.displayOverview.AJAXRefreshRows().
+   */
+  regionChange: function (region) {
+
+    // When triggered by a row drag, the 'format' select needs to be adjusted
+    // to the new region.
+    var currentValue = this.$formatSelect.val();
+    switch (region) {
+      case 'visible':
+        if (currentValue == 'hidden') {
+          // Restore the formatter back to the default formatter. Pseudo-fields do
+          // not have default formatters, we just return to 'visible' for those.
+          var value = (this.defaultFormatter != undefined) ? this.defaultFormatter : 'visible';
+        }
+        break;
+
+      default:
+        var value = 'hidden';
+        break;
+    }
+    if (value != undefined) {
+      this.$formatSelect.val(value);
+    }
+
+    var refreshRows = {};
+    if (region == 'hidden') {
+      refreshRows[this.name] = this.$formatSelect.get(0);
+    }
+
+    return refreshRows;
+  },
+};
+
+})(jQuery);
