104420bf8b4bc9ed24b97c3b4d33e10ee1d0a9d1
swentel <swentel@realize.be>
2010-10-30 23:16:03 +0000

    Template files can be used now for layouts

---
+++ layouts/ds_2col/ds-2col.tpl.php
@@ -0,0 +1,18 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * Display Suite 2 column template.
+ */
+?>
+
+<div class="group-left">
+  <?php print render($content['group_left']); ?>
+</div>
+
+<div class="group-right">
+  <?php print render($content['group_right']); ?>
+</div>
+
+<div class="clear-fix"></div>
---
+++ layouts/ds_2col_stacked/ds-2col-stacked.tpl.php
@@ -0,0 +1,24 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * Display Suite 2 column stacked template.
+ */
+?>
+
+<div class="group-header">
+  <?php print render($content['group_header']); ?>
+</div>
+
+<div class="group-left">
+  <?php print render($content['group_left']); ?>
+</div>
+
+<div class="group-right">
+  <?php print render($content['group_right']); ?>
+</div>
+
+<div class="group-footer">
+  <?php print render($content['group_footer']); ?>
+</div>
