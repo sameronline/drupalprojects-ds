a4962b6932b21fcc98666ce1e0a51d83be4a4656
swentel <swentel@realize.be>
2010-10-31 00:10:38 +0000

    Adding the 3 column

---
+++ layouts/ds_3col/ds-3col.tpl.php
@@ -0,0 +1,22 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * Display Suite 3 column template.
+ */
+?>
+
+<div class="group-left">
+  <?php print render($content['group_left']); ?>
+</div>
+
+<div class="group-middle">
+  <?php print render($content['group_middle']); ?>
+</div>
+
+<div class="group-right">
+  <?php print render($content['group_right']); ?>
+</div>
+
+<div class="clear-fix"></div>
---
+++ layouts/ds_3col_stacked/ds-3col-stacked.tpl.php
@@ -0,0 +1,28 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * Display Suite 3 column stacked template.
+ */
+?>
+
+<div class="group-header">
+  <?php print render($content['group_header']); ?>
+</div>
+
+<div class="group-left">
+  <?php print render($content['group_left']); ?>
+</div>
+
+<div class="group-middle">
+  <?php print render($content['group_middle']); ?>
+</div>
+
+<div class="group-right">
+  <?php print render($content['group_right']); ?>
+</div>
+
+<div class="group-footer">
+  <?php print render($content['group_left']); ?>
+</div>
