b0747c9dec1cc99e8e24e5b32ce24de6d757690f
Jan-yves Vanhaverbeke <jyve@591438.no-reply.drupal.org>
2011-01-12 09:23:56 +0000

    Template changes

--- layouts/ds_2col/ds-2col.tpl.php
+++ layouts/ds_2col/ds-2col.tpl.php
@@ -5,14 +5,17 @@
  * @file
  * Display Suite 2 column template.
  */
+
+// Get the region content, move this to the preprocess.
+$left = ds_render_region($content, 'left', $ds_layout);
+$right = ds_render_region($content, 'right', $ds_layout);
 ?>
-
-<div class="group-left">
-  <?php print ds_render_region($content, 'left', $ds_layout); ?>
+<div class="<?php print $classes;?> clearfix">
+  <div class="group-left">
+    <?php print $left; ?>
+  </div>
+  
+  <div class="group-right">
+    <?php print $right; ?>
+  </div>
 </div>
-
-<div class="group-right">
-  <?php print ds_render_region($content, 'right', $ds_layout); ?>
-</div>
-
-<div class="clear-fix"></div>
--- layouts/ds_3col/ds-3col.tpl.php
+++ layouts/ds_3col/ds-3col.tpl.php
@@ -3,20 +3,25 @@
 
 /**
  * @file
- * Display Suite 3 column template.
+ * Display Suite 3 column 25/50/25 template.
  */
+
+// Get the region content, move this to the preprocess.
+$middle = ds_render_region($content, 'middle', $ds_layout);
+$left = ds_render_region($content, 'left', $ds_layout);
+$right = ds_render_region($content, 'right', $ds_layout);
+
 ?>
+<div class="<?php print $classes;?> clearfix">
+  <div class="group-left">
+    <?php print $left; ?>
+  </div>
+  
+  <div class="group-middle">
+    <?php print $middle; ?>
+  </div>
 
-<div class="group-left">
-  <?php print ds_render_region($content, 'left', $ds_layout); ?>
+  <div class="group-right">
+    <?php print $right; ?>
+  </div>
 </div>
-
-<div class="group-middle">
-  <?php print ds_render_region($content, 'middle', $ds_layout); ?>
-</div>
-
-<div class="group-right">
-  <?php print ds_render_region($content, 'right', $ds_layout); ?>
-</div>
-
-<div class="clear-fix"></div>
