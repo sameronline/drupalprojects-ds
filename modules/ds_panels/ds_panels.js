c46a589f7188cf0337c8cdd1497cc3a08272f9ce
Kristof De Jaeger <swentel@realize.be>
2011-06-17 00:41:14 +0200

    Change layout works too now.

--- modules/ds_panels/ds_panels.js
+++ modules/ds_panels/ds_panels.js
@@ -5,7 +5,7 @@ Drupal.behaviors.PanelViewModes = {
   attach: function (context) {
     if ($('#edit-layout-remove').length > 0) {
       $('#edit-layout-remove').click(function() {
-        return confirm(Drupal.t('Are you sure you want to delete the layout ?'));
+        return confirm(Drupal.t('Are you sure you want to remove the layout ?'));
       });
     }
   }
