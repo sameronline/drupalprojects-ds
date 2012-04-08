a15492952e5dea8aee560db3542c1dc5b276cc0c
swentel <swentel@realize.be>
2012-04-08 13:20:57 +0200

    - #1520028 by swentel, aspilicious: Fix drush template generation.

--- example_layout/example-layout.tpl.php
+++ example_layout/example-layout.tpl.php
@@ -8,37 +8,42 @@
  * Layout:
  * - $classes: String of classes that can be used to style this layout.
  * - $contextual_links: Renderable array of contextual links.
+ * - $layout_wrapper: wrapper surrounding the layout.
  *
  * Regions:
  *
  * - $left: Rendered content for the "Left" region.
  * - $left_classes: String of classes that can be used to style the "Left" region.
+ * - $left_wrapper: wrapper surrounding the left region.
  *
  * - $right: Rendered content for the "Right" region.
  * - $right_classes: String of classes that can be used to style the "Right" region.
+ * - $right_wrapper: wrapper surrounding the right region.
  */
 ?>
-<div class="<?php print $classes; ?> clearfix">
+<<?php print $layout_wrapper ?> class="<?php print $classes;?> clearfix">
 
+  <!-- Needed to activate contextual links -->
   <?php if (isset($title_suffix['contextual_links'])): ?>
     <?php print render($title_suffix['contextual_links']); ?>
   <?php endif; ?>
 
   <!-- regions -->
 
-  <?php if ($left): ?>
-    <div class="ds-left<?php print $left_classes; ?>">
-      <?php print $left; ?>
-    </div>
-  <?php endif; ?>
+  <<?php print $left_wrapper ?> class="ds-left<?php print $left_classes; ?>">
+    <?php print $left; ?>
+  </<?php print $left_wrapper ?>>
 
-  <?php if ($right): ?>
-    <div class="ds-right<?php print $right_classes; ?>">
-      <?php print $right; ?>
-    </div>
-  <?php endif; ?>
+  <<?php print $right_wrapper ?> class="ds-right<?php print $right_classes; ?>">
+    <?php print $right; ?>
+  </<?php print $right_wrapper ?>>
 
   <!-- These comments are required for the Drush command. You can remove them in your own copy -->
   <!-- /regions -->
 
-</div>
+</<?php print $layout_wrapper ?>>
+
+<!-- Needed to activate display suite support on forms -->
+<?php if (!empty($drupal_render_children)): ?>
+  <?php print $drupal_render_children ?>
+<?php endif; ?>
