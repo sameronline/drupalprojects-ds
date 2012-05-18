35be805cffbbd8843b364b187fb7a3b0e8b1d1eb
swentel <swentel@realize.be>
2012-05-18 09:21:10 +0200

    - #1582246 by swentel: remove panel view modes. You can use Panelizer from now on.

--- ds.api.php
+++ ds.api.php
@@ -507,15 +507,6 @@ function hook_ds_label_options_alter(&$field_label_options) {
  */
 
 /**
- * Return fields to be added when creating a new display with the panels editor.
- */
-function hook_ds_panels_default_fields($entity_type, $bundle, $view_mode) {
-  // Get the fields from Field API.
-  $fields = field_info_instances($entity_type, $bundle);
-  return $fields;
-}
-
-/**
  * Alter the view mode just before it's rendered by the DS views entity plugin.
  *
  * @param $view_mode
