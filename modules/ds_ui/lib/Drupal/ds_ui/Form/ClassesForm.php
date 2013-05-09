dc2d73e4051b2eef706c25a321ec72a9c560d5d7
Bram Goffings <bramgoffings@gmail.com>
2013-05-09 23:21:31 +0200

    Implement form controller for the classes configuration page

---
+++ modules/ds_ui/lib/Drupal/ds_ui/Form/ClassesForm.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\ds_ui\Form\ClassesForm.
+ */
+
+namespace Drupal\ds_ui\Form;
+
+use Drupal\system\SystemConfigFormBase;
+use Drupal\Core\Config\ConfigFactory;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Configures classes used by Display Suite.
+ */
+class ClassesForm extends SystemConfigFormBase {
+
+  /**
+   * Constructs a \Drupal\ds_ui\ClassessForm object.
+   *
+   * @param \Drupal\Core\Config\ConfigFactory $config_factory
+   *   The factory for configuration objects.
+   */
+  public function __construct(ConfigFactory $config_factory) {
+    $this->configFactory = $config_factory;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static($container->get('config.factory'));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormID() {
+    return 'ds_classes_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, array &$form_state) {
+
+    $config = $this->configFactory->get('ds.classes');
+
+    $form['regions'] = array(
+      '#type' => 'textarea',
+      '#title' => t('CSS classes for regions'),
+      '#default_value' => $config->get('regions'),
+      '#description' => t('Configure CSS classes which you can add to regions on the "manage display" screens. Add multiple CSS classes line by line.<br />If you want to have a friendly name, separate class and friendly name by |, but this is not required. eg:<br /><em>class_name_1<br />class_name_2|Friendly name<br />class_name_3</em>')
+    );
+
+    $form['fields'] = array(
+      '#type' => 'textarea',
+      '#title' => t('CSS classes for fields'),
+      '#default_value' => $config->get('fields'),
+      '#description' => t('Configure CSS classes which you can add to fields on the "manage display" screens. Add multiple CSS classes line by line.<br />If you want to have a friendly name, separate class and friendly name by |, but this is not required. eg:<br /><em>class_name_1<br />class_name_2|Friendly name<br />class_name_3</em>')
+    );
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, array &$form_state) {
+    parent::submitForm($form, $form_state);
+
+    $config = $this->configFactory->get('ds.classes');
+    $config->set('regions', $form_state['values']['regions'])
+      ->set('fields', $form_state['values']['fields'])
+      ->save();
+  }
+
+}
