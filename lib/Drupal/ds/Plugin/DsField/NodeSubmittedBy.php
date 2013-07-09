32b6e688024e9d70e8a5145f0509194933af9ffb
Bram Goffings <bramgoffings@gmail.com>
2013-07-09 13:14:52 +0200

    Fixed submitted by DsField

--- lib/Drupal/ds/Plugin/DsField/NodeSubmittedBy.php
+++ lib/Drupal/ds/Plugin/DsField/NodeSubmittedBy.php
@@ -26,14 +26,14 @@ class NodeSubmittedBy extends Date {
    * Overrides \Drupal\ds\Plugin\ds\field\Date::renderField().
    */
   public function renderField($field) {
-    $account = user_load($field['entity']->uid);
+    $account = user_load($field['entity']->uid->value);
     switch ($field['formatter']) {
       case 'ds_time_ago':
-        $interval = REQUEST_TIME - $field['entity']->created;
+        $interval = REQUEST_TIME - $field['entity']->created->value;
         return t('Submitted !interval ago by !user.', array('!interval' => format_interval($interval), '!user' => theme('username', array('account' => $account))));
       default:
         $date_format = str_replace('ds_post_date_', '', $field['formatter']);
-        return t('Submitted by !user on !date.', array('!user' => theme('username', array('account' => $account)), '!date' => format_date($field['entity']->created, $date_format)));
+        return t('Submitted by !user on !date.', array('!user' => theme('username', array('account' => $account)), '!date' => format_date($field['entity']->created->value, $date_format)));
     }
   }
 
