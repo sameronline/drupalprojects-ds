8e3c88d0b0abec9a844ba85e2bce6010b4296c5b
Bram Goffings <bramgoffings@gmail.com>
2013-07-25 17:23:14 +0200

    fixed menu items

--- lib/Drupal/ds/Plugin/Menu/LocalTask/ListTask.php
+++ lib/Drupal/ds/Plugin/Menu/LocalTask/ListTask.php
@@ -14,7 +14,7 @@ use Drupal\Core\Annotation\Translation;
 /**
  * @LocalTask(
  *   id = "ds_list_task",
- *   route_name = "ds_structure",
+ *   route_name = "ds_list",
  *   title = @Translation("List"),
  *   tab_root_id = "ds_display_task",
  *   tab_parent_id = "ds_display_task"
