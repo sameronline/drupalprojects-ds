315d55dbb15beee79ce81931dda796eae9f2b92a
Bram Goffings <bramgoffings@gmail.com>
2013-07-26 11:50:47 +0200

    Moved and fixed field template js parsing

--- js/ds.admin.js
+++ js/ds.admin.js
@@ -25,7 +25,7 @@ Drupal.fieldUIDisplayOverview.ds = function (row, data) {
   this.$regionSelect = $('select.ds-field-region', row);
   this.$regionSelect.change(Drupal.fieldUIOverview.onChange);
 
-  // Attach change listener to the 'formatter type' select.
+  // Attach change listener to the 'plugin type' select.
   this.$formatSelect = $('select.field-plugin-type', row);
   this.$formatSelect.change(Drupal.fieldUIOverview.onChange);
 
@@ -75,6 +75,79 @@ Drupal.fieldUIDisplayOverview.ds.prototype = {
      }
 
      return refreshRows;
+  }
+};
+
+/**
+ * Field template.
+ */
+Drupal.behaviors.settingsToggle = {
+  attach: function (context) {
+
+    // Bind on click.
+    $('.field-plugin-settings-edit-form', context).once('ds-ft', function() {
+
+      var fieldTemplate = $(this);
+
+      // Bind on field template select button.
+      fieldTemplate.find('.ds-extras-field-template').change(function() {
+        ds_show_expert_settings(fieldTemplate);
+      });
+
+      ds_show_expert_settings(fieldTemplate);
+
+    });
+
+    // Show / hide settings on field template form.
+    function ds_show_expert_settings(element, open) {
+      field = element;
+      ft = $('.ds-extras-field-template', field).val();
+
+      if (ft == 'theme_ds_field_expert') {
+        // Show second, third, fourth, fifth and sixth label.
+        if ($('.lb .form-item:nth-child(1)', field).is(':visible')) {
+          $('.lb .form-item:nth-child(2), .lb .form-item:nth-child(3), .lb .form-item:nth-child(4), .lb .form-item:nth-child(5), .lb .form-item:nth-child(6)', field).show();
+        }
+        // Remove margin from update button.
+        $('.ft-update', field).css({'margin-top': '-10px'});
+        // Show wrappers.
+        $('.ow, .fis, .fi', field).show();
+      }
+      else {
+        // Hide second, third, fourth, fifth and sixth  label.
+        $('.lb .form-item:nth-child(2), .lb .form-item:nth-child(3), .lb .form-item:nth-child(4), .lb .form-item:nth-child(5), .lb .form-item:nth-child(6)', field).hide();
+        // Add margin on update button.
+        $('.ft-update', field).css({'margin-top': '10px'});
+        // Hide wrappers.
+        $('.ow, .fis, .fi', field).hide();
+      }
+
+      // Colon.
+      if (ft == 'theme_field' || ft == 'theme_ds_field_reset') {
+        $('.colon-checkbox', field).parent().hide();
+      }
+      else if ($('.lb .form-item:nth-child(1)', field).is(':visible')) {
+        $('.colon-checkbox', field).parent().show();
+      }
+
+      // CSS classes.
+      if (ft != 'theme_ds_field_expert' && ft != 'theme_ds_field_reset') {
+        $('.field-classes', field).show();
+      }
+      else {
+        $('.field-classes', field).hide();
+      }
+    }
+
+    $('.label-change').change(function() {
+      var field = $(this).parents('tr');
+      if ($('.field-template', field).length > 0) {
+        ft = $('.ds-extras-field-template', field).val();
+        if (ft == 'theme_field' || ft == 'theme_ds_field_reset') {
+          $('.colon-checkbox', field).parent().hide();
+        }
+      }
+    });
   }
 };
 
