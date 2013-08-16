99e2b5d5dc825d6f5a8724a49885913ba9ef1458
Bram Goffings <bramgoffings@gmail.com>
2013-08-16 22:33:27 +0200

    Remove dpm's

--- lib/Drupal/ds/Plugin/Derivative/DynamicField.php
+++ lib/Drupal/ds/Plugin/Derivative/DynamicField.php
@@ -32,12 +32,8 @@ abstract class DynamicField extends DerivativeBase {
             'properties' => $field['properties'],
             'entity_type' => $entity_type,
           );
-          dpm($field);
           if (!empty($field['ui_limit'])) {
             $this->derivatives[$key]['ui_limit'] = explode("\n", $field['ui_limit']);
-
-            dpm('wtf');
-            dpm($this->derivatives[$key]);
             // Ensure that all strings are trimmed, eg. don't have extra spaces,
             // \r chars etc.
             foreach ($this->derivatives[$key]['ui_limit'] as $k => $v) {
