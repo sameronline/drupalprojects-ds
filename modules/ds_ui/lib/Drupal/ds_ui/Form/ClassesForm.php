6247469af65ae48e9bfa90a60a5eb1da0d257efa
Bram Goffings <bramgoffings@gmail.com>
2013-12-31 16:06:32 +0100

    Fixed various config issues

--- modules/ds_ui/lib/Drupal/ds_ui/Form/ClassesForm.php
+++ modules/ds_ui/lib/Drupal/ds_ui/Form/ClassesForm.php
@@ -25,19 +25,19 @@ class ClassesForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, array &$form_state) {
-    $config = $this->configFactory->get('ds.classes');
+    $config = $this->configFactory->get('ds.settings');
 
     $form['regions'] = array(
       '#type' => 'textarea',
       '#title' => t('CSS classes for regions'),
-      '#default_value' => implode("\n", $config->get('regions')),
+      '#default_value' => implode("\n", $config->get('classes.region')),
       '#description' => t('Configure CSS classes which you can add to regions on the "manage display" screens. Add multiple CSS classes line by line.<br />If you want to have a friendly name, separate class and friendly name by |, but this is not required. eg:<br /><em>class_name_1<br />class_name_2|Friendly name<br />class_name_3</em>')
     );
 
     $form['fields'] = array(
       '#type' => 'textarea',
       '#title' => t('CSS classes for fields'),
-      '#default_value' =>  implode("\n", $config->get('fields')),
+      '#default_value' =>  implode("\n", $config->get('classes.field')),
       '#description' => t('Configure CSS classes which you can add to fields on the "manage display" screens. Add multiple CSS classes line by line.<br />If you want to have a friendly name, separate class and friendly name by |, but this is not required. eg:<br /><em>class_name_1<br />class_name_2|Friendly name<br />class_name_3</em>')
     );
 
@@ -49,9 +49,22 @@ class ClassesForm extends ConfigFormBase {
    */
   public function submitForm(array &$form, array &$form_state) {
     parent::submitForm($form, $form_state);
-    $config = $this->configFactory->get('ds.classes');
-    $config->set('regions', explode("\n", str_replace("\r", '', $form_state['values']['regions'])))
-      ->set('fields', explode("\n", str_replace("\r", '', $form_state['values']['fields'])))
+
+    // Prepare region classes
+    $region_classes = array();
+    if (!empty($form_state['values']['regions'])) {
+      $region_classes = explode("\n", str_replace("\r", '', $form_state['values']['regions']));
+    }
+
+    // Prepare field classes
+    $field_classes = array();
+    if (!empty($form_state['values']['fields'])) {
+      $field_classes = explode("\n", str_replace("\r", '', $form_state['values']['fields']));
+    }
+
+    $config = $this->configFactory->get('ds.settings');
+    $config->set('classes.region', $region_classes)
+      ->set('classes.field', $field_classes)
       ->save();
   }
 
