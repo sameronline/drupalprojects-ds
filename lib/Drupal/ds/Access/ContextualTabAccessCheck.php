a765fbbcb6bcbb91e2e039f358eaad98ad47cac6
Bram Goffings <bramgoffings@gmail.com>
2014-01-01 16:02:56 +0100

    Fixes for access checks according to the new standards

--- lib/Drupal/ds/Access/ContextualTabAccessCheck.php
+++ lib/Drupal/ds/Access/ContextualTabAccessCheck.php
@@ -7,7 +7,7 @@
 
 namespace Drupal\ds\Access;
 
-use Drupal\Core\Access\StaticAccessCheckInterface;
+use Drupal\Core\Routing\Access\AccessInterface as RoutingAccessInterface;
 use Drupal\Core\Session\AccountInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Route;
@@ -15,14 +15,7 @@ use Symfony\Component\Routing\Route;
 /**
  * Provides an access check for ds extras switch field routes.
  */
-class ContextualTabAccessCheck implements StaticAccessCheckInterface {
-
-  /**
-   * {@inheritdoc}
-   */
-  public function appliesTo() {
-    return array('_access_ds_contextual_tab');
-  }
+class ContextualTabAccessCheck implements RoutingAccessInterface {
 
   /**
    * {@inheritdoc}
