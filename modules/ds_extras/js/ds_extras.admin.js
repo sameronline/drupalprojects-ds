708ebbe0c1dc0bb5f3aabe948c2e38e39850c6cc
aspilicious <aspilicious@172527.no-reply.drupal.org>
2014-01-07 15:33:22 +0100

    Issue #1824348 by alanburke, aspilicious | carajito: Label wrapper.

--- modules/ds_extras/js/ds_extras.admin.js
+++ modules/ds_extras/js/ds_extras.admin.js
@@ -77,7 +77,7 @@ Drupal.behaviors.settingsToggle = {
         // Remove margin from update button.
         $('.ft-update', field).css({'margin-top': '-10px'});
         // Show wrappers.
-        $('.ow, .fis, .fi', field).show();
+        $('.lbw, .ow, .fis, .fi', field).show();
       }
       else {
         // Hide second, third, fourth, fifth and sixth  label.
@@ -85,7 +85,7 @@ Drupal.behaviors.settingsToggle = {
         // Add margin on update button.
         $('.ft-update', field).css({'margin-top': '10px'});
         // Hide wrappers.
-        $('.ow, .fis, .fi', field).hide();
+        $('.lbw, .ow, .fis, .fi', field).hide();
       }
 
       // Colon.
