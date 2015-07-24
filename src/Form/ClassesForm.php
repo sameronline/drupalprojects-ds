ca2a51796d8329bbc64b5ae4799a43374f1889cb
bceyssens <bceyssens@2743951.no-reply.drupal.org>
2015-07-24 10:03:37 +0200

    Issue #2499703 by bceyssens: Hide field classes option when DS Extras is not enabled

--- src/Form/ClassesForm.php
+++ src/Form/ClassesForm.php
@@ -35,12 +35,15 @@ class ClassesForm extends ConfigFormBase {
       '#description' => t('Configure CSS classes which you can add to regions on the "manage display" screens. Add multiple CSS classes line by line.<br />If you want to have a friendly name, separate class and friendly name by |, but this is not required. eg:<br /><em>class_name_1<br />class_name_2|Friendly name<br />class_name_3</em>')
     );
 
-    $form['fields'] = array(
-      '#type' => 'textarea',
-      '#title' => t('CSS classes for fields'),
-      '#default_value' =>  implode("\n", $config->get('classes.field')),
-      '#description' => t('Configure CSS classes which you can add to fields on the "manage display" screens. Add multiple CSS classes line by line.<br />If you want to have a friendly name, separate class and friendly name by |, but this is not required. eg:<br /><em>class_name_1<br />class_name_2|Friendly name<br />class_name_3</em>')
-    );
+    // Only show field classes if DS extras module is enabled
+    if (\Drupal::moduleHandler()->moduleExists('ds_extras')) {
+      $form['fields'] = array(
+        '#type' => 'textarea',
+        '#title' => t('CSS classes for fields'),
+        '#default_value' =>  implode("\n", $config->get('classes.field')),
+        '#description' => t('Configure CSS classes which you can add to fields on the "manage display" screens. Add multiple CSS classes line by line.<br />If you want to have a friendly name, separate class and friendly name by |, but this is not required. eg:<br /><em>class_name_1<br />class_name_2|Friendly name<br />class_name_3</em>')
+      );
+    }
 
     return parent::buildForm($form, $form_state);
   }
