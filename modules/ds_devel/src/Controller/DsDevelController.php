792aecd210bb7be3df52b9919c72c44d71fda81f
Bram Goffings <bramgoffings@gmail.com>
2015-11-21 22:02:07 +0100

    Fixed ds devel integration

--- modules/ds_devel/src/Controller/DsDevelController.php
+++ modules/ds_devel/src/Controller/DsDevelController.php
@@ -8,6 +8,7 @@
 namespace Drupal\ds_devel\Controller;
 
 use Drupal\Component\Utility\Html;
+use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Url;
 use Drupal\node\Entity\Node;
 
@@ -19,38 +20,43 @@ class DsDevelController {
   /**
    * Lists all instances of fields on any views.
    *
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *    A RouteMatch object.
+   *
    * @return array
    *   The Views fields report page.
    */
-  public function nodeMarkup($node, $key = 'default') {
-    $node = Node::load($node);
+  public function entityMarkup(RouteMatchInterface $route_match) {
 
-    $builded_entity = entity_view($node, $key);
-    $markup = drupal_render($builded_entity);
+    $parameter_name = $route_match->getRouteObject()->getOption('_devel_entity_type_id');
+    $entity = $route_match->getParameter($parameter_name);
+    $entity_type_id = $entity->getEntityTypeId();
+
+    $key = \Drupal::request()->get('key', 'default');
+
+    $builded_entity = entity_view($entity, $key);
+    $markup = \Drupal::service('renderer')->render($builded_entity);
 
     $links = array();
-    $links['default'] = array(
-      'title' => 'Default',
-      'url' => Url::fromRoute('ds_devel.markup', array('node' => $node->id())),
-    );
-    $view_modes = \Drupal::entityManager()->getViewModes('node');
-    foreach ($view_modes as $id => $info) {
-      if (!empty($info['status'])) {
-        $links[] = array(
-          'title' => $info['label'],
-          'url' => Url::fromRoute('ds_devel.markup_view_mode', array('node' => $node->id(), 'key' => $id)),
-        );
-      }
+    $active_view_modes = \Drupal::entityManager()->getViewModeOptionsByBundle($entity_type_id, $entity->bundle());
+    foreach ($active_view_modes as $id => $label) {
+      $links[] = array(
+        'title' => $label,
+        'url' => Url::fromRoute("entity.$entity_type_id.devel_markup", array($entity_type_id => $entity->id(), 'key' => $id)),
+      );
     }
 
     $build['links'] = array(
       '#theme' => 'links',
       '#links' => $links,
-      '#prefix' => '<div>',
+      '#prefix' => '<hr/><div>',
       '#suffix' => '</div><hr />',
     );
     $build['markup'] = [
       '#markup' => '<code><pre>' . Html::escape($markup) . '</pre></code>',
+      '#cache' => array(
+        'max-age' => 0,
+      ),
       '#allowed_tags' => ['code', 'pre'],
     ];
 
