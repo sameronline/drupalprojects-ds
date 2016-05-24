191d5a1d7b31d8ef721788a4c3c6e48e1702ae7c
aspilicious <aspilicious@172527.no-reply.drupal.org>
2016-05-24 22:35:53 +0200

    Issue #2727383 by aspilicious: Display Suite + Entityqueue display problem

--- src/Tests/ManageDisplayTabTest.php
+++ src/Tests/ManageDisplayTabTest.php
@@ -24,23 +24,23 @@ class ManageDisplayTabTest extends FastTestBase {
     // Verify we can see the manage display tab on a node and can click on it
     $this->drupalGet('node/' . $node->id());
     $this->assertRaw('Manage display', 'Manage display tab title found on node');
-    $this->assertRaw('node/' . $node->id() . '/display', 'Manage display tab link found on node');
+    $this->assertRaw('node/' . $node->id() . '/manage_display', 'Manage display tab link found on node');
     // @todo figure out why this crashes
-    //$this->drupalGet('node/' . $node->id() . '/display');
+    //$this->drupalGet('node/' . $node->id() . '/manage_display');
 
     // Verify we can see the manage display tab on a user and can click on it
     $this->drupalGet('user/' . $this->adminUser->id());
     $this->assertRaw('Manage display', 'Manage display tab title found on user');
-    $this->assertRaw('user/' . $this->adminUser->id() . '/display', 'Manage display tab link found on user');
+    $this->assertRaw('user/' . $this->adminUser->id() . '/manage_display', 'Manage display tab link found on user');
     // @todo figure out why this crashes
-    //$this->drupalGet('user/' .  $this->adminUser->id() . '/display');
+    //$this->drupalGet('user/' .  $this->adminUser->id() . '/manage_display');
 
     // Verify we can see the manage display tab on a taxonomy term and can click on it
     $this->drupalGet('taxonomy/term/1');
     $this->assertRaw('Manage display', 'Manage display,title tab found on term');
-    $this->assertRaw('taxonomy/term/1/display', 'Manage display tab link found on term');
+    $this->assertRaw('taxonomy/term/1/manage_display', 'Manage display tab link found on term');
     // @todo figure out why this crashes
-    //$this->drupalGet('taxonomy/term/1/display');
+    //$this->drupalGet('taxonomy/term/1/manage_display');
 
   }
 
