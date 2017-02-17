1970fdec684d7150917a75f5c95001fb30633ef2
Bram Goffings <bramgoffings@gmail.com>
2017-02-17 10:04:25 +0100

    Fixed problem with non matching arguments

--- src/Plugin/DsField/DsFieldBase.php
+++ src/Plugin/DsField/DsFieldBase.php
@@ -48,7 +48,7 @@ abstract class DsFieldBase extends PluginBase implements DsFieldInterface {
   /**
    * {@inheritdoc}
    */
-  public function settingsSummary($settings) {
+  public function settingsSummary(array $settings) {
     return array();
   }
 
